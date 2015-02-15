7b3d0ecc64c00e65272c892401108c1797fa2bb6
Nick Colley <nickcolley7@gmail.com>
2015-02-15 21:06:08 +0000

    made it purtier

--- index.js
+++ index.js
@@ -6,24 +6,30 @@
 var MirrorConsole = require("./lib/mirror-console");
 var PouchDB = require('pouchdb');
 var content = document.querySelector(".content");
-var editor = new MirrorConsole();
+var editor = new MirrorConsole({ theme: "monokai" });
 editor.setText(content.textContent);
 editor.swapWithElement(content);
+
+function line(text) {
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode(text));
+    return div;
+}
+document.getElementById("clear").addEventListener("click", function () {
+   document.getElementById("output").innerHTML = '';
+});
 
 document.getElementById("eval").addEventListener("click", function () {
     var consoleMock = {
         log: function (arg) {
-            function line(text) {
-                var div = document.createElement("div");
-                div.appendChild(document.createTextNode(text));
-                return div;
-            }
-
-            document.getElementById("output").appendChild(line(arg));
+            var $output = document.getElementById("output")
+            $output.insertBefore(line(arg), $output.firstChild);
         }
     }
     editor.runInContext({ PouchDB: PouchDB, console: consoleMock },function (error, result) {
         if (error) {
+            var $output = document.getElementById("output")
+            $output.insertBefore(line(arg), $output.firstChild);
             console.error(error);
         }
     });
